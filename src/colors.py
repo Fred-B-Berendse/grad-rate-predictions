985b3ced0f821840f9c25d7a64e51e1d8843d4c1
fberendse0710 <fbb0710@gmail.com>
2019-11-05 11:46:45 -0700

    added get_colors, make_color_dict

--- src/colors.py
+++ src/colors.py
@@ -1,14 +1,30 @@
 import numpy as np
-from plotting import make_color_dict
 from matplotlib import cm
+
+
+def make_color_dict(labels, cmap, start=0, end=1):
+    '''
+    Makes a dictionary of colors for each key in keys
+    '''
+    colors = cmap(np.linspace(start, end, len(labels)))
+    return dict(zip(labels, colors))
+
+
+def get_colors(labels, color_dict):
+    '''
+    Extracts a list of colors from a color dictionary in the order of labels
+    '''
+    results = []
+    [results.append(color_dict[l]) for l in labels]
+    return np.array(results)
 
 
 def targets_color_dict():
     labels = ['Asian', 'Black', 'Hispanic', 'Nat. Am.', 'Pac. Isl.', 'White',
               '2+ Races']
-    labels = np.array(['Graduation Rate: ' + l for l in labels]) 
+    # labels = np.array(['Graduation Rate: ' + l for l in labels]) 
     color_dict = make_color_dict(labels, cm.Accent)
     labels = ['Pell Grant', 'SSL', 'Non-Recipient']
-    labels = np.array(['Graduation Rate: ' + l for l in labels])
+    # labels = np.array(['Graduation Rate: ' + l for l in labels])
     color_dict.update(make_color_dict(labels, cm.brg))
     return color_dict
