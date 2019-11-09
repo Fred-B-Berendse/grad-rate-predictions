3ce8c6246207106807b4c0dad21a9f355e1171a4
fberendse0710 <fbb0710@gmail.com>
2019-11-08 21:18:03 -0700

    implemented joblib, added desc_dict

--- src/forest.py
+++ src/forest.py
@@ -8,6 +8,7 @@ from regressor import Regressor
 from copy import deepcopy
 from colors import targets_color_dict
 from dataset import Dataset
+from joblib import dump, load
 plt.style.use('seaborn-whitegrid')
 plt.style.use('seaborn-poster')
 
@@ -37,7 +38,8 @@ class ForestRegressor(Regressor):
             ax.tick_params(axis='x', rotation=70)
             ax.set_title(target)
 
-    def plot_partial_dependences(self, features_list, targets_list):
+    def plot_partial_dependences(self, features_list, targets_list, 
+                                 desc_dict=None):
         model = deepcopy(self.model)
         feat_idx = [np.where(self.dataset.feature_labels == f)
                     for f in features_list]
@@ -59,7 +61,10 @@ class ForestRegressor(Regressor):
 
         for i, f in enumerate(features_list):
             axi = ax[i // 2, i % 2]
-            axi.set_xlabel(f)
+            if desc_dict is not None:
+                axi.set_xlabel(desc_dict[f])
+            else:
+                axi.set_xlabel(f)
             axi.legend(loc='best')
         plt.tight_layout()
 
@@ -82,25 +87,10 @@ class ForestRegressor(Regressor):
 
 if __name__ == "__main__":
 
+    do_grid_search = False
+
     mdf = pd.read_csv('data/ipeds_2017_cats_eda.csv')
     mdf.drop(['Unnamed: 0', 'applcn'], axis=1, inplace=True)
-
-    # Original features
-    # feat_cols = ['iclevel_2to4', 'iclevel_0to2', 'iclevel_na',
-    #              'control_public', 'control_privnp', 'control_na',
-    #              'hloffer_assoc', 'hloffer_doct', 'hloffer_bach',
-    #              'hloffer_mast', 'hloffer_2to4yr', 'hloffer_0to1yr',
-    #              'hloffer_postmc', 'hloffer_na', 'hloffer_postbc',
-    #              'hbcu_yes', 'tribal_yes', 'locale_ctylrg', 'locale_ctysml',
-    #              'locale_ctymid', 'locale_twndst', 'locale_rurfrg',
-    #              'locale_twnrem', 'locale_submid', 'locale_subsml',
-    #              'locale_twnfrg', 'locale_rurdst', 'locale_rurrem',
-    #              'locale_na', 'instsize_1to5k', 'instsize_5to10k',
-    #              'instsize_10to20k', 'instsize_na', 'instsize_gt20k',
-    #              'instsize_norpt', 'landgrnt_yes', 'longitud', 'latitude',
-    #              'admssn_pct', 'enrlt_pct', 'enrlft_pct', 'en25', 'en75',
-    #              'mt25', 'mt75', 'uagrntp', 'upgrntp', 'npgrn2', 
-    #              'grnton2_pct', 'grntof2_pct', 'grntwf2_pct']
 
     # Surviving features after VIF elimination
     feat_cols = np.array(['control_privnp', 'hloffer_postmc', 'hloffer_postbc',
@@ -113,11 +103,6 @@ if __name__ == "__main__":
                           'latitude', 'admssn_pct', 'enrlt_pct', 'enrlft_pct',
                           'en25', 'uagrntp', 'upgrntp', 'npgrn2',
                           'grntof2_pct', 'grntwf2_pct'])
-
-    # # Test of capstone 2 features
-    # feat_cols = np.array(['longitud', 'latitude', 'admssn_pct', 'enrlt_pct',
-    #                       'enrlft_pct', 'en25', 'uagrntp', 'upgrntp',
-    #                       'grntof2_pct', 'grntwf2_pct'])
 
     target_cols = np.array(['cstcball_pct_gr2mort', 'cstcball_pct_grasiat',
                             'cstcball_pct_grbkaat', 'cstcball_pct_grhispt',
@@ -147,38 +132,29 @@ if __name__ == "__main__":
     rfbase.print_metrics()
     print("\n")
 
-    # # Use Grid Search to find best hyperparameters
-    # grid = {'n_estimators': [int(x) for x in np.linspace(100, 200, 11)],
-    #         'criterion': ['mse', 'mae'],
-    #         'min_samples_split': [int(x) for x in np.linspace(2, 10, 2)],
-    #         'max_features': ['auto', 'sqrt']}
+    if do_grid_search:
 
-    # search_model = GridSearchCV(base_model, grid,
-    #                             scoring=None, n_jobs=-1, iid=False,
-    #                             refit=True, cv=5, verbose=2,
-    #                             pre_dispatch='2*n_jobs',
-    #                             error_score=np.nan,
-    #                             return_train_score=False)
-    # rfsearch = ForestRegressor(search_model, ds)
-    # rfsearch.fit_train()
-    # print("Best Hyperparameters: ")
-    # print(rfsearch.model.best_params_)
+        # Use Grid Search to find best hyperparameters
+        grid = {'n_estimators': [int(x) for x in np.linspace(100, 200, 11)],
+                'criterion': ['mse', 'mae'],
+                'min_samples_split': [int(x) for x in np.linspace(2, 10, 2)],
+                'max_features': ['auto', 'sqrt']}
 
-    # Best model from grid search
-    # {'criterion': 'mse', 'max_features': 'sqrt',
-    #  'min_samples_split': 2, 'n_estimators': 160}
-
-    best_model = RandomForestRegressor(n_estimators=160, criterion='mse',
-                                       min_samples_split=2,
-                                       min_samples_leaf=1,
-                                       min_weight_fraction_leaf=0.0,
-                                       max_features='sqrt',
-                                       max_leaf_nodes=None,
-                                       min_impurity_decrease=0.0,
-                                       bootstrap=True,
-                                       oob_score=False, n_jobs=-1,
-                                       random_state=10, warm_start=False)
-    # best_model = search_model.best_estimator_
+        search_model = GridSearchCV(base_model, grid,
+                                    scoring=None, n_jobs=-1, iid=False,
+                                    refit=True, cv=5, verbose=2,
+                                    pre_dispatch='2*n_jobs',
+                                    error_score=np.nan,
+                                    return_train_score=False)
+        rfsearch = ForestRegressor(search_model, ds)
+        rfsearch.fit_train()
+        print("Best Hyperparameters: ")
+        print(rfsearch.model.best_params_)
+        best_model = rfsearch.model.best_estimator_
+        # Use joblib instead of pickle to save the model
+        dump(best_model, 'data/forest.joblib')
+    else:
+        best_model = load('data/forest.joblib')
 
     rfbest = ForestRegressor(best_model, ds)
     rfbest.fit_train()
@@ -190,11 +166,16 @@ if __name__ == "__main__":
     rfbest.plot_feature_importances(n_features=8)
     plt.show()
 
+    desc_dict = {'en25': 'English 25th Percentile', 
+                 'upgrntp': 'Percent Receiving Pell Grant',
+                 'admssn_pct': 'Percent of Applicants Admitted',
+                 'grntwf2_pct': 'Percent Living with Family'}
+
     top_features = ['en25', 'upgrntp', 'admssn_pct', 'grntwf2_pct']
     targets = ['2+ Races', 'Asian', 'Black', 'Hispanic', 'White']
-    rfbest.plot_partial_dependences(top_features, targets)
+    rfbest.plot_partial_dependences(top_features, targets, desc_dict=desc_dict)
     plt.show()
 
     targets = ['Pell Grant', 'SSL', 'Non-Recipient']
-    rfbest.plot_partial_dependences(top_features, targets)
+    rfbest.plot_partial_dependences(top_features, targets, desc_dict=desc_dict)
     plt.show()
