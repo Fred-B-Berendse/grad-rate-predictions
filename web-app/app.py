d8279002d65940435ae3a8e3b23d4c7cf3e368b6
fberendse0710 <fbb0710@gmail.com>
2019-11-09 18:56:15 -0700

    sanitized SQL inputs

--- web-app/app.py
+++ web-app/app.py
@@ -1,73 +1,34 @@
-from flask import Flask, render_template, request, json
-import requests
-import webbrowser, threading, os
+from flask import Flask, render_template, request
 import numpy as np
 import pandas as pd
-from pymongo import MongoClient
-from datapoints import DataPoint
-from model import Model
-
-client = MongoClient()
+from database import Database
 # from model import Model
 
 app = Flask(__name__)
 
-def pipeline():
-
-    dp = DataPoint()
-    data_point = dp.retrieve()
-
-    # Fake fraud results
-    # fraud_prob = round(np.random.random(), 2)
-    # fraud_flag = fraud_prob > 0.5
-
-    mod = Model()
-    mod.load_model('models/model_all_features_fin.pkl')
-    fraud_flag, fraud_prob = mod.preds_new_data_point(data_point)    
-
-    # merge our results with the original JSON event object
-    results = {'fraud_flag': fraud_flag,
-               'fraud_prob': fraud_prob,
-               'event': dp.json_data} # event 
-    results_copy = results.copy()
-    client = MongoClient('mongodb://localhost:27017')
-    db = client['fraud_db'] 
-    events = db.events
-    db_result = events.insert_one(results)
-    print('One post: {0}'.format(db_result.inserted_id))
-    print('Acknowledged: {}'.format(db_result.acknowledged))
-    return results_copy
+ratesdb = Database(local=False)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # Get names for each institution
+    sql_str = 'SELECT unitid, instnm, city, stabbr ' + \
+        'FROM institutions ORDER BY instnm ASC;'
+    inst_df = ratesdb.from_sql_query(sql_str)
+    return render_template('index.html',
+                           unitid=inst_df['unitid'],
+                           instnm=inst_df['instnm'],
+                           city=inst_df['city'],
+                           stabbr=inst_df['stabbr'],
+                           len=len(inst_df['unitid']))
+
 
 @app.route("/visualize", methods=['POST'])
-def visualize():   
-    # response = requests.get('http://galvanize-case-study-on-fraud.herokuapp.com/data_point')
-    # event = response.json()
-    results = pipeline()
-    return render_template('visualize.html', 
-                           object_id=results['event']['object_id'],
-                           event_name=results['event']['name'],
-                           org_name=results['event']['org_name'],
-                           description=results['event']['description'],
-                           fraud_flag=results['fraud_flag'], 
-                           fraud_prob=results['fraud_prob'])
-
-@app.route("/api", methods=['GET', 'POST'])
-def api():
-    # response = requests.get('http://galvanize-case-study-on-fraud.herokuapp.com/data_point')
-    # event = response.json()
-    results = pipeline()
-    response = app.response_class(
-        response=json.dumps(results),
-        status=200,
-        mimetype='application/json'
-    )
-
-    return response
+def visualize():
+    unitid = str(request.form['institution'])
+    sql_str = 'SELECT * FROM institutions WHERE unitid = :unitid'
+    inst_df = ratesdb.from_sql_query(sql_str, unitid=unitid)
+    return render_template('visualize.html', data=inst_df)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8105, threaded=True)
+    app.run(host='0.0.0.0', port=8105, threaded=True, debug=True)
---
+++ web-app/database.py
@@ -0,0 +1,57 @@
+import os
+import sqlalchemy as db
+from sqlalchemy import text
+import pandas as pd
+
+
+class Database(object):
+
+    def __init__(self, local=True):
+
+        if local:
+            host = 'localhost'
+            port = '5435'
+            user = 'postgres'
+            pwd = ''
+        else:
+            host = 'database-1.cwtci684dbqt.us-west-1.rds.amazonaws.com'
+            port = '5432'
+            user = os.environ.get('AWS_RDS_USERNAME')
+            pwd = os.environ.get('AWS_RDS_PASSWORD')
+        self.hoststring = \
+            'postgresql://{}:{}@{}:{}/gradrates'.format(user, pwd, host, port)
+        self.engine = db.create_engine(self.hoststring)
+
+    def to_sql(self, df, db_table_name):
+        df.to_sql(db_table_name, self.engine, if_exists='replace')
+
+    def from_sql(self, table_name):
+        # reads a SQL table into a Pandas dataframe 
+        # see pandas.read_sql
+        print('reading {} from database'.format(table_name))
+        metadata = db.MetaData()
+        table = db.Table(table_name, metadata, autoload=True, 
+                         autoload_with=self.engine)
+        query = db.select([table])
+        return self.from_sql_query(query)
+
+    def from_sql_query(self, sql_str, **kwargs):
+        # reads a SQL query into a dataframe
+        # https://towardsdatascience.com/sqlalchemy-python-tutorial-79a577141a91
+        sql = text(sql_str)
+        ResultProxy = self.engine.execute(sql, **kwargs)
+        ResultSet = ResultProxy.fetchall()
+        result = pd.DataFrame(data=ResultSet, columns=ResultSet[0].keys())
+        return result
+
+    def close(self):
+        self.engine.dispose()
+
+
+if __name__ == "__main__":
+
+    print("Connecting to database")
+    ratesdb = Database(local=False)
+    unitid = 188429
+    sql_str = 'SELECT * FROM institutions WHERE unitid = :unitid'
+    inst_df = ratesdb.from_sql_query(sql_str, unitid=unitid)
